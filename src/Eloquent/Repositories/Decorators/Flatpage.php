28e0186d25d126eca3823f266f5d900d53396c9d
Matthew Daly <matthewbdaly@gmail.com>
2017-11-14 19:33:04 +0000

    Added outlines of repositories

---
+++ src/Eloquent/Repositories/Decorators/Flatpage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Matthewbdaly\LaravelFlatpages\Eloquent\Repositories\Decorators;
+
+use Matthewbdaly\LaravelRepositories\Repositories\Decorators\BaseDecorator;
+
+class Flatpage extends BaseDecorator
+{
+    //
+}
---
+++ src/Eloquent/Repositories/Flatpage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Matthewbdaly\LaravelFlatpages\Eloquent\Repositories;
+
+use Matthewbdaly\LaravelRepositories\Repositories\Base;
+
+class Flatpage extends Base
+{
+    //
+}
