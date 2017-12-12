434b9edefd538daab0535284b27dab683819bb9a
Matthew Daly <matthewbdaly@gmail.com>
2017-12-12 22:29:00 +0000

    Removed CSS from base view
    Caused problems when including the view

--- src/views/base.blade.php
+++ src/views/base.blade.php
@@ -4,8 +4,6 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
-        <meta name="theme-color" content="#1565C0">
-        <link href="{{ elixir('css/app.min.css') }}" rel="stylesheet" type="text/css">
         <title>{{ flatpage.title }}</title>
     </head>
     <body>
