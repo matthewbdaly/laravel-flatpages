eb26f9e0d0ddca9b6a3810d762df3209d80cbe89
Matthew Daly <matthewbdaly@gmail.com>
2018-01-03 10:42:10 +0000

    Can now set custom views
    The flatpage model's template field can now be either left blank to use
    the default template, or can set the name of a different template.

--- src/Http/Controllers/FlatpageController.php
+++ src/Http/Controllers/FlatpageController.php
@@ -38,6 +38,9 @@ class FlatpageController extends BaseController
     public function page($path)
     {
         $page = $this->repository->findBySlug("/$path/");
-        return view('flatpages::base', ['flatpage' => $page]);
+        if (!$template = $page->template) {
+            $template = 'flatpages::base';
+        }
+        return view($template, ['flatpage' => $page]);
     }
 }
--- tests/Unit/Http/Controllers/FlatpageControllerTest.php
+++ tests/Unit/Http/Controllers/FlatpageControllerTest.php
@@ -14,13 +14,36 @@ class FlatpageControllerTest extends TestCase
         $flatpage->shouldReceive('toArray')->once()->andReturn([
             'id' => 1,
             'title' => 'About me',
-            'content' => 'All about me'
+            'content' => 'All about me',
+            'template' => null,
         ]);
+        $flatpage->shouldReceive('getAttribute')->with('template')->once()->andReturn(null);
         $repo = m::mock('Matthewbdaly\LaravelFlatpages\Contracts\Repositories\Flatpage');
         $repo->shouldReceive('findBySlug')->with('/about/')->once()->andReturn($flatpage);
         $controller = new FlatpageController($repo);
         $response = $controller->page('about');
         $this->assertEquals('flatpages::base', $response->getName());
+        $data = $response->getData()['flatpage']->toArray();
+        $this->assertEquals(1, $data['id']);
+        $this->assertEquals('About me', $data['title']);
+        $this->assertEquals('All about me', $data['content']);
+    }
+    
+    public function testGetFlatpageWithCustomTemplate()
+    {
+        $flatpage = m::mock('Matthewbdaly\LaravelFlatpages\Eloquent\Models\Flatpage');
+        $flatpage->shouldReceive('toArray')->once()->andReturn([
+            'id' => 1,
+            'title' => 'About me',
+            'content' => 'All about me',
+            'template' => 'flatpages::custom'
+        ]);
+        $flatpage->shouldReceive('getAttribute')->with('template')->once()->andReturn('flatpages::custom');
+        $repo = m::mock('Matthewbdaly\LaravelFlatpages\Contracts\Repositories\Flatpage');
+        $repo->shouldReceive('findBySlug')->with('/about/')->once()->andReturn($flatpage);
+        $controller = new FlatpageController($repo);
+        $response = $controller->page('about');
+        $this->assertEquals('flatpages::custom', $response->getName());
         $data = $response->getData()['flatpage']->toArray();
         $this->assertEquals(1, $data['id']);
         $this->assertEquals('About me', $data['title']);
